cf3aafd9b8875dc0718cb784e0c15269400ca6d9
shantk54 <shantkeshishian54@gmail.com>
2025-09-10 14:11:02 -0400

    New rule

--- src/main/java/shant/rules/DooRule.java
+++ src/main/java/shant/rules/DooRule.java
@@ -5,7 +5,7 @@ public class DooRule extends TransliterationRule {
     public RuleResult apply(char[] chars, int i) {
         CharInfo charInfo = getCharInfo(chars, i);
 
-        if (i == 0 && chars.length == 3 && charInfo.current() == 'դ' && chars[1] == 'ո' && chars[2] == 'ւ') {
+        if (i + 2 < chars.length && charInfo.current() == 'դ' && chars[i + 1] == 'ո' && chars[i + 2] == 'ւ') {
             return new RuleResult(formatOutput("doo", charInfo.isUpperCase()), 2);
         }
         return null;
