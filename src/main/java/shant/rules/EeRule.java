e2261a73a8276d51be0442a063a0176659b82ae5
shantk54 <shantkeshishian54@gmail.com>
2025-09-11 12:55:22 -0400

    New rules and new test data

--- src/main/java/shant/rules/EeRule.java
+++ src/main/java/shant/rules/EeRule.java
@@ -2,12 +2,13 @@ package shant.rules;
 
 import shant.utils.Utils;
 
-public class EeRule extends TransliterationRule{
+public class EeRule extends TransliterationRule {
     @Override
     public RuleResult apply(char[] chars, int i) {
         CharInfo charInfo = getCharInfo(chars, i);
 
-        if (charInfo.current() == 'ի' && Utils.isStartOfWord(chars, i)) {
+        if (charInfo.current() == 'ի' &&
+                (Utils.isStartOfWord(chars, i) || Utils.isEndOfWord(chars, i))) {
             return new RuleResult(formatOutput("ee", charInfo.isUpperCase()), 0);
         }
         return null;
